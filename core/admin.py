9d22b40d7329c07e48410082b08f8f6ebadd85bd
root <ruslan.yupyn@outlook.com>
2018-12-29 17:45:12 -0800

    admin.py updated

--- core/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-
-class CoreMemberAdmin(admin.ModelAdmin):
-    search_fields = ['name']
-    list_display = ['name', 'role']
-
-admin.site.register(models.CoreMember, CoreMemberAdmin)
